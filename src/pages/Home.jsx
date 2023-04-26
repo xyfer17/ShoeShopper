a7d86125da2bc50e1ffd990286a1e0f09537074a
xyfer17 <nav3044999@gmail.com>
2023-04-26 19:15:45 +0530

    add slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Helmet from "../components/Helmet";
 import { Col, Row } from "reactstrap";
 import "../styles/home.css";
 import ProductCard from "../components/UI/ProductCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
+  const sliderRef = React.useRef(); // <-- (1) create Ref
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    draggable: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <Helmet title="Nike. Just do It. Nike">
       <div className="home_container">
@@ -31,18 +64,69 @@ const Home = () => {
           <Col lg="12">
             <div className="sale_tagline d-flex align-items-center justify-content-between">
               <span>The Latest & Greatest</span>
-              <span>Shop</span>
+
+              <div className="slider_button d-flex align-items-center gap-3">
+                <span>Shop</span>
+
+                <button
+                  onClick={() => {
+                    sliderRef.current.slickPrev();
+                  }}
+                >
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    viewBox="0 0 24 24"
+                    role="img"
+                    width="24px"
+                    height="24px"
+                    fill="none"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-width="1.5"
+                      d="M15.525 18.966L8.558 12l6.967-6.967"
+                    ></path>
+                  </svg>
+                </button>
+                <button
+                  onClick={() => {
+                    sliderRef.current.slickNext();
+                  }}
+                >
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    viewBox="0 0 24 24"
+                    role="img"
+                    width="24px"
+                    height="24px"
+                    fill="none"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-width="1.5"
+                      d="M8.474 18.966L15.44 12 8.474 5.033"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
             </div>
           </Col>
-          <Col lg="4">
-            <ProductCard />
-          </Col>
-          <Col lg="4">
-            <ProductCard />
-          </Col>
-          <Col lg="4">
-            <ProductCard />
-          </Col>
+          <Slider className="product_scroll" ref={sliderRef} {...settings}>
+            <Col lg="4">
+              <ProductCard />
+            </Col>
+            <Col lg="4">
+              <ProductCard />
+            </Col>
+            <Col lg="4">
+              <ProductCard />
+            </Col>
+            <Col lg="4">
+              <ProductCard />
+            </Col>
+          </Slider>
         </Row>
       </div>
     </Helmet>
